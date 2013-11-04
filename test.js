cdc4c91324b6c7791308c85b70fb3f873dd91bf6
leenian <leenian@gmail.com>
2013-11-04 11:46:13 +0800

    仅作测试而已。

--- test.js
+++ test.js
@@ -2,6 +2,17 @@
 
 (function(){
    var Test = function(){
-       var a="1";
+       var that = this;
+       //初始化
+       that.options = {};
+       that.data =[];
+       that.init = function(options,data){
+           that.options  = options||{};
+           this.data  =  data;
+           return new Date();
+       }
+       this.setData = function(){
+            return null;
+       }
    } 
  })();
